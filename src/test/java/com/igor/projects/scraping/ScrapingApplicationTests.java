c2ef69656a6f84edaefaae4632530b374a14897d
igorbavand <igor.bvn@gmail.com>
2022-11-15 13:33:47 -0300

    first commit

---
+++ src/test/java/com/igor/projects/scraping/ScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.projects.scraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
